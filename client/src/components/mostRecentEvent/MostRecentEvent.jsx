bbe8a4ba510bac240394cfa691f7a40b3bdee734
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-06-30 22:52:38 +0600

    add: most recent Events in home page

---
+++ client/src/components/mostRecentEvent/MostRecentEvent.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import baseUrl from '../../service/baseUrl';
+import { useAuthContext } from '../../context/authContext';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import EventCard from '../ui/eventCard/EventCard';
+
+const MostRecentEvent = () => {
+
+    const [events, setEvents] = useState([]);
+    const { authUser } = useAuthContext();
+
+    const handleJoin = async (eventId) => {
+        const userId = authUser?._id;
+
+        try {
+            await axios.post(`${baseUrl}/api/events/join/${eventId}`, { userId });
+            toast.success('Successfully joined the event');
+            fetchEvents();
+        } catch (error) {
+            toast.error(error.response?.data?.message || 'Failed to join event');
+        }
+    };
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const fetchEvents = async () => {
+        const res = await axios.get(`${baseUrl}/api/events`);
+        setEvents(res.data);
+    };
+
+    return (
+        <div className="space-y-6">
+            <h1 className="text-4xl font-bold text-gray-800 text-center">Most Recent Events</h1>
+
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {events.slice(0, 6).map((event) => (
+                    <EventCard key={event._id} event={event} onJoin={handleJoin} />
+                ))}
+            </div>
+        </div>
+
+    );
+};
+
+export default MostRecentEvent;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import HeaderBanner from '../../components/HeaderBanner/HeaderBanner';
+import MostRecentEvent from '../../components/mostRecentEvent/MostRecentEvent';
 
 const Home = () => {
     return (
         <div>
-            <HeaderBanner/>
+            <HeaderBanner />
+            <div className='px-8 md:px-24 mt-12'>
+                <MostRecentEvent />
+            </div>
         </div>
     );
 };
