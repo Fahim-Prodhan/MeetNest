d6c420f2733876b539c8cd0b5de3d97cdea631df
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-06-30 23:03:04 +0600

    mission and vision add in home page

---
+++ client/src/components/missionVision/MissionVision.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import img1 from '../../assets/images/mission.png';
+import img2 from '../../assets/images/vision.png';
+import 'aos/dist/aos.css';
+
+const MissionAndVision = () => {
+  return (
+    <div>
+      <h1 className="text-center pb-12 text-3xl md:text-4xl font-bold mt-24">Mission & Vision</h1>
+
+      <div
+        className="grid lg:grid-cols-2 items-center gap-6"
+      >
+        <div>
+          <h1 className="text-3xl font-black pb-4 text-[#F6B17A]">Our Mission</h1>
+          <p className="md:text-xl">
+            Our mission is to revolutionize the event planning experience by providing a seamless platform that empowers organizers to create, manage, and promote events efficiently. We strive to bring people together through impactful and memorable events that foster connection, creativity, and collaboration.
+          </p>
+        </div>
+        <div className="order-first lg:order-last">
+          <img src={img1} alt="Mission" />
+        </div>
+      </div>
+
+      <div
+        className="grid lg:grid-cols-2 items-center mt-12 gap-6"
+      >
+        <div>
+          <img src={img2} alt="Vision" />
+        </div>
+        <div>
+          <h1 className="text-3xl font-black pb-4 text-[#F6B17A]">Our Vision</h1>
+          <p className="md:text-xl">
+            Our vision is to be the leading platform for event innovation, where technology meets creativity to enable unforgettable experiences. We envision a world where anyone can effortlessly bring people together through events that inspire, educate, and celebrate, regardless of scale or purpose.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MissionAndVision;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderBanner from '../../components/HeaderBanner/HeaderBanner';
 import MostRecentEvent from '../../components/mostRecentEvent/MostRecentEvent';
+import MissionAndVision from '../../components/missionVision/MissionVision';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <HeaderBanner />
             <div className='px-8 md:px-24 mt-12'>
                 <MostRecentEvent />
+                <MissionAndVision/>
             </div>
         </div>
     );
